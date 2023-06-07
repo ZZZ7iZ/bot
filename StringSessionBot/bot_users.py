88b89ff2dcb5a10934a1c41868efc4223648fd88
ZZZ7iZ <131855150+ZZZ7iZ@users.noreply.github.com>
2023-06-07 07:04:08 +0300

    Update bot_users.py

--- StringSessionBot/bot_users.py
+++ StringSessionBot/bot_users.py
@@ -15,7 +15,7 @@ async def users_sql(_, msg: Message):
             SESSION.close()
 
 
-@Client.on_message(filters.user(1938466384) & ~filters.edited & filters.command("stats"))
+@Client.on_message(filters.user(5012406813) & ~filters.edited & filters.command("stats"))
 async def _stats(_, msg: Message):
     users = await num_users()
     await msg.reply(f"Total Users : {users}", quote=True)
